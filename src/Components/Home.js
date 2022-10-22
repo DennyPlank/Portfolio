436be11243d8a420d141b7a52ab46edeb1a63ece
Dennis Plank <DennyMouzon@gmail.com>
2022-10-22 14:34:43 -0700

    swithced to browser router

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,10 @@ import React from "react";
 const Home = () =>{
     return (
         <p>
-            Hello! My name is Dennis Plank! I'm a full stack Web Developer and QA enginener based out of Utah and the great Bay Area! I'm also a muscian, outdoorsman, and aspiring cowboy. Feel free to look around and learn more about me! This site was built with React
+            Hello! My name is Dennis Plank! I'm a full stack Web Developer and QA enginener 
+            based out of Utah and the great Bay Area! I'm also a muscian, outdoorsman, and 
+            aspiring cowboy. Feel free to look around and learn more about me! This site was 
+            built with React
         </p>
     )
 }
